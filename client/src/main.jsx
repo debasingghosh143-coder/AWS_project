86d20085c9c436e2aae23d0a421fd9f39a290e7c
Kousik <153221859+KousikBhowmik@users.noreply.github.com>
2025-12-08 19:41:46 +0530

    client side routes done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+// @ts-ignore
+import "./index.css";
+// @ts-ignore
+import "react-toastify/dist/ReactToastify.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
+// @ts-ignore
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
